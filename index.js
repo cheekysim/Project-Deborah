6710a28019ca2d7000001429b73ac7c6d6f1eef9
= <=>
2022-09-22 12:46:18 +0100

    updated intents

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 
 // Create a new collection of commands
 client.commands = new Collection();
